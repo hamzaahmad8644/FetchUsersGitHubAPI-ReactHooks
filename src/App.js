59fe4ca4620c0f21f16268d69e081ee5e4fd6ef7
Hamza Ahmad <hamzaahmad8644@gmail.com>
2021-10-01 07:50:33 +0500

    Fetch User from gitHubAPI completed

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./App.css";
+import UseEffectAPI from "./component/useEffect/useEffectAPI";
+
+const App = () => {
+  return <UseEffectAPI />;
+};
+
+export default App;
